6bf944c2871d70a2a8674b31bc1b6637d774e443
Eliza Pant <elizapant.github@gmail.com>
2023-11-02 20:43:00 +0545

    commit

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,28 +4,31 @@ import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
     return (
         <Container>
             <Row>
                 <Col md={5} className="login__bg"></Col>
                 <Col md={7} className="d-flex align-items-center justify-content-center flex-direction-column">
-                    <Form style={{ width: "80%", maxWidth: 500 }}>
+                <Form style={{ width: "80%", maxWidth: 500 }} onSubmit={handleLogin}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
+                            {error && <p className="alert alert-danger">{error.data}</p>}
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" required />
+                            <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} value={email} required />
                             <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" required />
+                            <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} required />
                         </Form.Group>
                         <Button variant="primary" type="submit">
-                            Login
+                            {isLoading ? <Spinner animation="grow" /> : "Login"}
                         </Button>
                         <div className="py-4">
                             <p className="text-center">
-                                Don't have an account? <Link to="/signup">Signup</Link>
+                                Don't have an account ? <Link to="/signup">Signup</Link>
                             </p>
                         </div>
                     </Form>
