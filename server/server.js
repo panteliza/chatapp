ef99d4c737daaae4fb9470e36be18f0c5f02f3bd
Eliza Pant <elizapant.github@gmail.com>
2023-11-06 00:06:29 +0545

    commit

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ const PORT = 5001;
 const io = require('socket.io')(server, {
   cors: {
     origin: 'http://localhost:5173',
-    methods: ['GET', 'POST']
+    methods: ['GET', 'POST'],
+    credentials:true
   }
 })
 
