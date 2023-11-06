088bc16694e995c3519ec3451811cb78300d25a8
Eliza Pant <elizapant.github@gmail.com>
2023-11-07 00:48:25 +0545

    commit

--- client/src/services/appApi.js
+++ client/src/services/appApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const appApi = createApi({
     reducerPath: "appApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://chatapp-api-nine.vercel.app/",
+        baseUrl: "http://localhost:5001",
         
     }),
    
