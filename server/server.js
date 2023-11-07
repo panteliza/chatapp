f6159306a2688b85df02121cbd7613c455a8253b
Eliza Pant <elizapant.github@gmail.com>
2023-11-07 11:49:42 +0545

    commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(cors());
+app.use(cors());  
 
 app.use('/users', userRoutes)
 require('./connection')
